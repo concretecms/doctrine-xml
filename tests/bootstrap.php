4c320196525dcb3b85215dcedd3aec7a83e037c7
Michele Locati <michele@locati.it>
2023-11-30 14:04:13 +0100

    Run tests on multiple DBAL versions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
 error_reporting(-1);
 
-if (!extension_loaded('xsl')) {
-    echo "The XSL PHP extension must be enabled.\n";
-    exit(1);
-}
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
